e1fbb1d01cf6bb74d18773c20d9207322f5be0a8
TuanVu <tuanthanh16@gmail.com>
2022-10-30 12:05:09 +0100

    integrate search bar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -33,6 +33,30 @@
         @can('admin')
             <span class="navbar_admin">Welcome Admin</span>
             <a href={{ route('admin.home') }}>Admin dashboard</a>
+            <div class="navbar_search">
+                <form action={{ route('product.search') }}>
+                    <div class="inner-form">
+                        <div class="input-field first-wrap">
+                            <div class="input-select">
+                                <select data-trigger="" name="category">
+                                    <option placeholder="">Category</option>
+                                    <option>New products</option>
+                                    <option>Sales</option>
+                                    <option>Foods</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="input-field second-wrap">
+                            <input name="keyword" placeholder="Enter Keywords?" type="text" />
+                        </div>
+                        <div class="input-field third-wrap">
+                            <button class="btn-search" type="submit">
+                                Search
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         @endcan
 
     </nav>
