b2bf59b0442f4490cc59d258c3e847557cd66710
Dijana <didimx.18@gmail.com>
2022-11-18 16:11:43 +0100

    title

--- resources/js/reactApp/Pages/AboutUs.jsx
+++ resources/js/reactApp/Pages/AboutUs.jsx
@@ -4,8 +4,8 @@ const AboutUs = () => {
     return (
         <section id="about">
             <div className="about-1">
-                <h1>About Us</h1>
-                <p>Lorem ipusm hahskjdfhksjdfhskjfhaskjdfhlksjdhflksjdfh</p>
+                <h1>Who we are</h1>
+                
             </div>
             <div className="about-2">
                 <div className="content-box-lg">
@@ -16,7 +16,7 @@ const AboutUs = () => {
                                     <i className="bi bi-globe-americas"></i>
                                     <h3>MISSION</h3>
                                     <hr />
-                                    <p>Our mission is to be a leader in the distribution and merchandising of food from the sunny Balkan directly to your table. We forge relationships with farmers who grow food in a natural way. </p>
+                                    <p>Our mission is to be a leader in the distribution and merchandising of food from the sunny Balkan directly to your table. We forge relationships with farmers who grow food in a natural way. We are devoted to using fresh ingredients with bold, wholesome and delicious flavours. We celebrate people's unique dietary needs and offer something for everyone. When you feel ready to try something new, come to our store.</p>
 
 
                                 </div>
@@ -24,9 +24,9 @@ const AboutUs = () => {
                             <div className="col-md-4">
                                 <div className="about-item text-center">
                                     <i className="bi bi-gem"></i>
-                                    <h3>Our Staff</h3>
+                                    <h3>Our history</h3>
                                     <hr />
-                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                    <p>This store was opened back in 2007 by a couple of settlers from the Balkan itself. We always felt nostalgic for our country. Then we thought of a way how to bring our culture closer to us - the local cuisine. When we taste these products, we always feel closer to home. We believe that food brings people together. It's our great fortune we also happen to come from a sunny region overlooking the Mediterranean!</p>
 
 
                                 </div>
@@ -34,9 +34,11 @@ const AboutUs = () => {
                             <div className="col-md-4">
                                 <div className="about-item text-center">
                                     <i className="bi bi-balloon-heart"></i>
-                                    <h3>MISSION</h3>
+                                    <h3>Where to find us?</h3>
                                     <hr />
-                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                    <p>The Balkan Discount Market is located in Krč, Prague 4, just 20 minutes by walk from Krčský les and within easy access of the metro C line, stop Budějovická.<br /><br />
+                                    A new branch coming up! <br />
+                                    We are working on opening the next branch for our customers in the lovely outskirts in Hloubětin, Prague 14, very close to the highway. Follow our social media for more updates.</p>
 
 
                                 </div>
