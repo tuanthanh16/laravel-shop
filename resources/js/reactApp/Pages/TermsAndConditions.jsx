f798de15df6577ce75deee9a260434440c014651
Dijana <didimx.18@gmail.com>
2022-11-22 15:24:43 +0100

    terms

--- resources/js/reactApp/Pages/TermsAndConditions.jsx
+++ resources/js/reactApp/Pages/TermsAndConditions.jsx
@@ -1,42 +1,46 @@
+import "../../../css/app.css";
 const TermsAndConditions = () => {
     return (
-        <div className="terms">
-            <h1>General Terms and Conditions</h1>
-            <p> The price of the goods and any costs associated with the delivery of the goods according to the purchase contract can be paid by the buyer in the following ways:
+        <div className="terms" style={{marginTop:"2rem"}}>
+            <h3>General Terms and Conditions</h3>
+            <p> The price of the goods and any costs associated with the delivery of the goods according to the purchase contract can be paid by the buyer in the following ways:<br/>
+            <ul>
+                <li>Cashless by payment card</li>
 
-                cashless by payment card
+               <li> In cash or by credit card for personal delivery by courier.</li>
 
-                in cash or by credit card for personal delivery by courier
+                </ul>
 
 
+                Along with the purchase price, the buyer is also obliged to pay the seller the costs associated with the packaging and delivery of the goods in the agreed amount. Unless otherwise stated, the purchase price is the price of the goods themselves, including the costs of packaging and delivery of the goods.<br/>
 
-                Along with the purchase price, the buyer is also obliged to pay the seller the costs associated with the packaging and delivery of the goods in the agreed amount. Unless otherwise stated, the purchase price is the price of the goods themselves, including the costs of packaging and delivery of the goods.
+                In the case of payment in cash, the purchase price is payable upon receipt of the goods. In the case of non-cash payment, the purchase price is payable within 14 days from the conclusion of the purchase contract.<br/>
 
-                In the case of payment in cash, the purchase price is payable upon receipt of the goods. In the case of non-cash payment, the purchase price is payable within 14 days from the conclusion of the purchase contract.
+                In the case of payment via a payment gateway, the buyer follows the instructions of the relevant electronic payment provider.<br/>
 
-                In the case of payment via a payment gateway, the buyer follows the instructions of the relevant electronic payment provider.
+                In the case of non-cash payment, the buyer's obligation to pay the purchase price is fulfilled when the relevant amount is credited to the seller's bank account.<br/>
 
-                In the case of non-cash payment, the buyer's obligation to pay the purchase price is fulfilled when the relevant amount is credited to the seller's bank account.
+                The seller does not require any advance payment or other similar payment from the buyer. Payment of the purchase price before the goods are shipped is not a deposit.<br/>
 
-                The seller does not require any advance payment or other similar payment from the buyer. Payment of the purchase price before the goods are shipped is not a deposit.
-
-                According to the Sales Registration Act, the seller is obliged to issue a receipt to the buyer. At the same time, he is obliged to register the received sales with the tax administrator online, in the event of a technical failure, then within 48 hours at the latest.
+                According to the Sales Registration Act, the seller is obliged to issue a receipt to the buyer. At the same time, he is obliged to register the received sales with the tax administrator online, in the event of a technical failure, then within 48 hours at the latest.<br/>
 
                 The goods are delivered to the buyer:
+                <ul>
 
-                to the address specified in the order
+                <li>to the address specified in the order</li>
 
-                through the delivery office to the delivery address specified by the buyer
+                <li>through the delivery office to the delivery address specified by the buyer</li>
+                </ul>
 
-                The buyer chooses the delivery method when ordering the goods.
+                The buyer chooses the delivery method when ordering the goods.<br/>
 
-                The costs of delivery of the goods, depending on the method of sending and receiving the goods, are indicated in the order and in its confirmation. In the event that the mode of transport is contracted on the basis of a special request of the buyer, the risk and any additional costs associated with this mode of transport shall be borne by the buyer.
+                The costs of delivery of the goods, depending on the method of sending and receiving the goods, are indicated in the order and in its confirmation. In the event that the mode of transport is contracted on the basis of a special request of the buyer, the risk and any additional costs associated with this mode of transport shall be borne by the buyer.<br/>
 
-                If, according to the purchase contract, the seller is obliged to deliver the goods to the place specified in the order, the buyer is obliged to take over the goods upon delivery. If, for reasons on the part of the buyer, it is necessary to deliver the goods repeatedly or in a different way than was specified in the order, the buyer is obliged to pay the costs associated with repeated delivery of the goods, or costs associated with another delivery method.
+                If, according to the purchase contract, the seller is obliged to deliver the goods to the place specified in the order, the buyer is obliged to take over the goods upon delivery. If, for reasons on the part of the buyer, it is necessary to deliver the goods repeatedly or in a different way than was specified in the order, the buyer is obliged to pay the costs associated with repeated delivery of the goods, or costs associated with another delivery method.<br/>
 
-                When taking over the goods from the carrier, the buyer is obliged to check the condition of the packaging of the goods and, in the event of any defects, notify the carrier of this fact immediately. In the event that the package is found to be damaged, which indicates an unauthorized intrusion into the shipment, the buyer is not obliged to accept the shipment from the carrier.
+                When taking over the goods from the carrier, the buyer is obliged to check the condition of the packaging of the goods and, in the event of any defects, notify the carrier of this fact immediately. In the event that the package is found to be damaged, which indicates an unauthorized intrusion into the shipment, the buyer is not obliged to accept the shipment from the carrier.<br/>
 
-                The seller issues a tax document - an invoice - to the buyer. The tax receipt is sent to the buyer's email address.
+                The seller issues a tax document - an invoice - to the buyer. The tax receipt is sent to the buyer's email address.<br/>
 
                 The buyer acquires the ownership right to the goods by paying the purchase price for the goods in full (including packaging and delivery costs), but first by taking over the goods. Responsibility for the destruction, damage or loss of the goods passes to the buyer at the time of acceptance of the goods or at the time when the buyer had the obligation to accept the goods, but did not do so in violation of the purchase contract.</p>
         </div>
