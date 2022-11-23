6bcdf92befe49cbf52583539d4378c5d1d35b5b7
TuanVu <tuanthanh16@gmail.com>
2022-11-23 12:17:23 +0100

    add csrf token in meta

--- resources/views/reactApp/index.blade.php
+++ resources/views/reactApp/index.blade.php
@@ -18,6 +18,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
+    <meta content="{{ csrf_token() }}" name="csrf-token">
     <title>Balkan Discount Market</title>
 </head>
 
