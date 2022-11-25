891d60eb80eb0079df02c2daf9bd9baebfb7ccd5
Dijana <didimx.18@gmail.com>
2022-11-25 10:19:46 +0100

    font changes

--- resources/views/reactApp/index.blade.php
+++ resources/views/reactApp/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta content="IE=edge" http-equiv="X-UA-Compatible">
     {{-- <meta content="width=device-width, initial-scale=1.0" name="viewport"> --}}
-    <link href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
 
     {{-- <meta content="width=device-width, initial-scale=1.0" name="viewport"> --}}
     <script src="https://kit.fontawesome.com/d4817eaa43.js" crossorigin="anonymous"></script>
